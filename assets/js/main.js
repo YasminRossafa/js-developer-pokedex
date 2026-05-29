d63bb0a2ac7e3eba60cde33ca3a431fcf79c468e
Yasmin Rossafa <70644604+YasminRossafa@users.noreply.github.com>
2026-05-29 10:55:11 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onclick="window.location.href='/pokemon-detail.html?id=${pokemon.number}'" title="Ver detalhes de ${pokemon.name}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
